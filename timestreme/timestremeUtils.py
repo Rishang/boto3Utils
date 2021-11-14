7bd2dde5b2e9ba97a05cfd04b82e8b9a21136045
Rishang <rishangbhavsar22@gmail.com>
2021-11-14 22:14:48 +0530

    update

--- timestreme/timestremeUtils.py
+++ timestreme/timestremeUtils.py
@@ -14,8 +14,9 @@ class TimeStreamRead:
     def message(self, msg, debug):
         if debug:
             print(msg)
+        return
 
-    def run_query(self, query_string, max_items=10, debug: bool = True):
+    def run_query(self, query_string, max_items=10, debug: bool = True, filter: bool=False, **kwargs):
 
         try:
             paginator = self.client.get_paginator("query")
@@ -30,30 +31,35 @@ class TimeStreamRead:
             data: list = []
             for page in page_iterator:
                 self.message(page, debug=debug)
-                data.append(page)
+                if filter == True:
+                    row = self.read_rows(page, **kwargs)
+                    data.append(row)
+                else:
+                    data.append(page)
             return data
 
         except Exception as err:
             print("Exception while running query:", err)
             traceback.print_exc(file=sys.stderr)
 
+    def _merge_dicts(self, l: list):
+        f = {}
+        for D in l:
+            for key, value in D.items():
+                f[key] = value
+        return f
+
     def read_rows(self, data, filter_column=[], to_dimention=False, nullarg=None):
-        def merge_dicts(l: list):
-            f = {}
-            for D in l:
-                for key, value in D.items():
-                    f[key] = value
-            return f
 
         rows = []
 
         for columns in data["Rows"]:
             tmp = []
             c_info = data["ColumnInfo"]
+            c_data = columns["Data"]
 
             if len(filter_column) != 0:
 
-                c_data = columns["Data"]
                 for c in range(len(c_info)):
                     column = c_info[c]["Name"]
                     if column in filter_column:
@@ -68,15 +74,7 @@ class TimeStreamRead:
                                 }
                             )
 
-                if to_dimention == False:
-                    f = merge_dicts(tmp)
-                    rows.append(f)
-                else:
-                    rows.append(tmp)
-
             else:
-
-                c_data = columns["Data"]
                 for c in range(len(c_data)):
                     if "NullValue" in c_data[c].keys():
                         if to_dimention == False:
@@ -94,13 +92,53 @@ class TimeStreamRead:
                                 }
                             )
 
-                if to_dimention == False:
-                    f = merge_dicts(tmp)
-                    rows.append(f)
-                else:
-                    rows.append(tmp)
-
+            if to_dimention == False:
+                f = self._merge_dicts(tmp)
+                rows.append(f)
+            else:
+                rows.append(tmp)
+        
         return rows
+
+    def add_col(self, table, columns: dict):
+        """
+        add extra column to
+        """
+
+        f = []
+        for row in table:
+            for name in columns:
+                row[name] = columns[name]
+            f.append(row)
+        return f
+
+    def to_dimention(self, d: dict):
+        """
+        Dimention contains data of
+        each row in timestreme database
+        """
+
+        dim = []
+        for i in d:
+            dim.append({"Name": i, "Value": d[i]})
+        return dim
+
+    def to_table(self, rows, epoc_time):
+        """
+        Table contains records or each dimention/rows
+        """
+
+        records = []
+        for row in rows:
+            record = {
+                "Time": epoc_time,
+                "Dimensions": self.to_dimention(row),
+                "MeasureName": "Machine_Tag",
+                "MeasureValue": "Machine_Value",
+                "MeasureValueType": "VARCHAR",
+            }
+            records.append(record)
+        return records
 
 
 class TimeStreamWrite:
@@ -108,24 +146,12 @@ class TimeStreamWrite:
     TimeStream Write utils
     """
 
+    record_chunk: bool = False
+    response: list = []
+
     def __init__(self, boto_session):
         self.sess = boto_session
         self.client = self.sess.client("timestream-write")
-
-    def add_col(self, dimentions, columns: dict):
-        f = []
-        for row in dimentions:
-            for name in columns:
-                row[name] = columns[name]
-            f.append(row)
-        return f
-
-    def to_dimention(self, d: dict):
-
-        dim = []
-        for i in d:
-            dim.append({"Name": i, "Value": d[i]})
-        return dim
 
     def nested_chunk(self, data_list: list, chunk_size: int):
         i = 0
@@ -136,20 +162,34 @@ class TimeStreamWrite:
 
         return new_list
 
-    def table(self, rows, r_time):
-        records = []
-        for row in rows:
-            record = {
-                "Time": r_time,
-                "Dimensions": self.to_dimention(row),
-                "MeasureName": "Machine_Tag",
-                "MeasureValue": "Machine_Value",
-                "MeasureValueType": "VARCHAR",
-            }
-            records.append(record)
-        return records
+    def write_records(self, db_name, table_name, records, chunk_size=None):
+        """
+        writes given records rows
+        to defined `table_name` of database `db_name`
+        on aws timestreme.
+        """
 
+        def _write(records):
+            result = self.client.write_records(
+                DatabaseName=db_name,
+                TableName=table_name,
+                Records=records,
+                CommonAttributes={},
+            )
+            print(
+                "WriteRecords Status: [%s]"
+                % result["ResponseMetadata"]["HTTPStatusCode"]
+            )
+            return result
 
+        if self.record_chunk == True:
+            nested_rec = self.nested_chunk(records, chunk_size)
+            for rec in nested_rec:
+                resp = _write(rec)
+                self.response.append(resp)
+        else:
+            resp = _write(records)
+            self.response.append(resp)
 
 
 class ViewDictTable:
@@ -168,7 +208,7 @@ class ViewDictTable:
     pd.DataFrame(t_see)
 
     """
-    
+
     def view_dimention(self, dimention):
         f = {}
         for dim in dimention:
